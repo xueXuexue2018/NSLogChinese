e9d85ec943b9cba61af5d0692c06ea1d4ce83938
XJL <xjl@XJLdeMacBook-Pro.local>
2018-09-14 15:29:40 +0800

    Initial Commit

---
+++ 控制台输出中文/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  控制台输出中文
+//
+//  Created by XJL on 2018/9/14.
+//  Copyright © 2018年 XJL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 控制台输出中文/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  控制台输出中文
+//
+//  Created by XJL on 2018/9/14.
+//  Copyright © 2018年 XJL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
